f6844c6a3188faec81b24bf2d6912b893f3a7487
Wellerson <wellerson.developer@gmail.com>
2021-06-25 23:08:20 -0300

    feat: init relay config

---
+++ packages/client/src/relay/Environment.tsx
@@ -0,0 +1,25 @@
+// import { installRelayDevTools } from 'relay-devtools';
+import { Environment, Network, RecordSource, Store } from 'relay-runtime';
+import { relayTransactionLogger } from './relayTransactionLogger';
+
+import cacheHandler from './cacheHandler';
+
+const __DEV__ = process.env.NODE_ENV === 'development';
+// if (__DEV__) {
+//   installRelayDevTools();
+// }
+
+const network = Network.create(cacheHandler);
+
+const source = new RecordSource();
+const store = new Store(source);
+
+// export const inspector = new RecordSourceInspector(source);
+
+const env = new Environment({
+  network,
+  store,
+  log: __DEV__ ? relayTransactionLogger : null,
+});
+
+export default env;
---
+++ packages/client/src/relay/createQueryRendererModern.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import hoistStatics from 'hoist-non-react-statics';
+import { QueryRenderer, GraphQLTaggedNode } from 'react-relay';
+import { Variables } from 'relay-runtime'
+
+import Environment from './Environment';
+
+type Config = {
+  query: GraphQLTaggedNode,
+  queriesParams?: (props: Object) => object,
+  variables?: Variables,
+  hideSplash?: boolean,
+};
+
+export default function createQueryRenderer<P>(
+  FragmentComponent: any,
+  Component: React.ComponentType<P>,
+  config: Config,
+) {
+  const { query, queriesParams } = config;
+
+  class QueryRendererWrapper extends React.Component<{}> {
+    render() {
+      const variables = queriesParams ? queriesParams(this.props) : config.variables;
+
+      return (
+        <QueryRenderer
+          environment={Environment}
+          query={query}
+          variables={variables || {}}
+          render={({ error, props }) => {
+            if (error) {
+              return <span>{error.toString()}</span>;
+            }
+
+            if (props) {
+              return <FragmentComponent {...this.props} query={props} />;
+            }
+
+            return <span>loading</span>;
+          }}
+        />
+      );
+    }
+  }
+
+  return hoistStatics(QueryRendererWrapper, Component);
+}
