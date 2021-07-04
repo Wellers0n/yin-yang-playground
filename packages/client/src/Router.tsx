a883474c7222a982364dc9ed2a168662ff6cc1c8
Wellerson <wellerson.developer@gmail.com>
2021-07-04 18:31:47 -0300

    feat: added fallback and lazy

--- packages/client/src/Router.tsx
+++ packages/client/src/Router.tsx
@@ -1,29 +1,30 @@
-import * as React from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import PrivateRouter from './components/PrivateRouter'
-import Login from './routes/Login'
-import Register from './routes/Register'
-import Home from './routes/Home'
+import React, { Suspense } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import PrivateRouter from "./components/PrivateRouter";
+
+const Login = React.lazy(() => import("./routes/Login"));
+const Register = React.lazy(() => import("./routes/Register"));
+const Home = React.lazy(() => import("./routes/Home"));
+const Edit = React.lazy(() => import("./routes/Edit"));
 
 const router = () => {
-    return (
-        <BrowserRouter>
-            <div>
-                <Switch>
-                    <Route path="/" exact component={Login} />
-                    <Route path="/register" component={Register} />
-                    <PrivateRouter path="/home" component={Home} />
-                    <Route component={NotFoundScreen} />
-                </Switch>
-            </div>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Suspense fallback={"Loading..."}>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/register" component={Register} />
+          <PrivateRouter path="/home" component={Home} />
+          <PrivateRouter path="/edit/:id" component={Edit} />
+          <Route component={NotFoundScreen} />
+        </Switch>
+      </Suspense>
+    </BrowserRouter>
+  );
+};
 
 const NotFoundScreen = () => {
-    return (
-        <div>page not found</div>
-    )
-}
+  return <div>page not found</div>;
+};
 
-export default router
+export default router;
