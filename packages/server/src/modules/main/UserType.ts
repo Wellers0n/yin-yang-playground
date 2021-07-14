a1c16c892dcc180348ed06d187d43adc3962ecc2
Wellerson <wellerson.developer@gmail.com>
2021-07-14 04:42:55 -0300

    feat: used connectionDefinitions

--- packages/server/src/modules/main/UserType.ts
+++ packages/server/src/modules/main/UserType.ts
@@ -2,33 +2,43 @@ import {
   GraphQLObjectType,
   GraphQLString,
   GraphQLID,
-  GraphQLList,
+  GraphQLNonNull,
 } from "graphql";
 import { globalIdField } from "graphql-relay";
+import { connectionDefinitions } from "../../core/connection/CustomConnectionType";
+// import { connectionDefinitions, globalIdField} from "graphql-relay";
 
-export default new GraphQLObjectType({
-  name: "Users",
+const UserType = new GraphQLObjectType({
+  name: "User",
+  description: "User data",
   fields: () => ({
-    id: globalIdField("Users"),
+    id: globalIdField("User"),
     _id: {
       type: GraphQLID,
+      resolve: (user) => user._id,
     },
     name: {
       type: GraphQLString,
+      resolve: (user) => user.name,
     },
     email: {
       type: GraphQLString,
+      resolve: (user) => user.email,
     },
     password: {
       type: GraphQLString,
+      resolve: (user) => user.password,
     },
     description: {
       type: GraphQLString,
+      resolve: (user) => user.description,
     },
   }),
 });
 
-// export const UserConnection = connectionDefinitions({
-//   name: "User",
-//   nodeType: GraphQLNonNull(UserType),
-// });
+export default UserType;
+
+export const UserConnection = connectionDefinitions({
+  name: "User",
+  nodeType: GraphQLNonNull(UserType),
+});
