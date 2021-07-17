0cef69bb5ac6c15695e8ac978ffb5bc25af95fe2
Wellerson <wellerson.developer@gmail.com>
2021-07-17 05:08:34 -0300

    feat: added connection in UserList

--- packages/client/src/components/UserList/index.tsx
+++ packages/client/src/components/UserList/index.tsx
@@ -5,22 +5,27 @@ import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { useHistory } from "react-router-dom";
 import { graphql, useFragment, commitMutation } from "react-relay";
 import Environment from "../../relay/Environment";
-import { UserList_query$key } from "./__generated__/UserList_query.graphql";
-import { UserListQueryResponse } from "./__generated__/UserListQuery.graphql";
+import { UserList_query, UserList_query$key } from "./__generated__/UserList_query.graphql";
+import { UserListMutationResponse } from "./__generated__/UserListMutation.graphql";
 
 type Props = {
-  query: UserList_query$key;
+  query: UserList_query;
 };
 
 const UserList = (props: Props) => {
   const data = useFragment<UserList_query$key>(
     graphql`
       fragment UserList_query on QueryType {
-        users {
-          _id
-          name
-          email
-          description
+        users(first: 10) @connection(key: "UserList_users", filters: []) {
+          edges {
+            node {
+              _id
+              id
+              name
+              description
+              email
+            }
+          }
         }
       }
     `,
@@ -60,7 +65,7 @@ const UserList = (props: Props) => {
       mutation,
       variables: { input: { id } },
       updater,
-      onCompleted: (response: UserListQueryResponse, errors: any) => {
+      onCompleted: (response: UserListMutationResponse, errors: any) => {
         if (errors) return console.log(errors);
       },
       onError: (err) => console.error(err),
@@ -69,13 +74,13 @@ const UserList = (props: Props) => {
 
   return (
     <Container>
-      {users?.map((value, index) => {
+      {users?.edges?.map((value, index) => {
         return (
           <Box key={index}>
             <div className="left">
               <FontAwesomeIcon
                 onClick={() =>
-                  history.push(`/edit/${value?._id}`, { id: value?._id })
+                  history.push(`/edit/${value?.node._id}`, { id: value?.node?._id })
                 }
                 icon={faEdit}
               />
@@ -85,9 +90,9 @@ const UserList = (props: Props) => {
               />
             </div>
             <div className="right">
-              <Name>{value?.name}</Name>
-              <Description>{value?.description}</Description>
-              <Email>{value?.email}</Email>
+              <Name>{value?.node?.name}</Name>
+              <Description>{value?.node?.description}</Description>
+              <Email>{value?.node?.email}</Email>
             </div>
           </Box>
         );
