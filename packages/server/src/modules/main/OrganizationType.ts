2ee72fcf48770da821601dabc4999ce1f6137512
Wellerson <wellerson.developer@gmail.com>
2021-07-18 02:51:34 -0300

    feat: added organizationType in modules

---
+++ packages/server/src/modules/main/OrganizationType.ts
@@ -0,0 +1,38 @@
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLNonNull,
+} from "graphql";
+import { connectionDefinitions, globalIdField } from "graphql-relay";
+
+const OrganizationType = new GraphQLObjectType({
+  name: "Organization",
+  description: "Organization data",
+  fields: () => ({
+    id: globalIdField("Organization"),
+    _id: {
+      type: GraphQLID,
+      resolve: (organization) => organization._id,
+    },
+    name: {
+      type: GraphQLString,
+      resolve: (organization) => organization.name,
+    },
+    email: {
+      type: GraphQLString,
+      resolve: (organization) => organization.email,
+    },
+    description: {
+      type: GraphQLString,
+      resolve: (organization) => organization.description,
+    },
+  }),
+});
+
+export default OrganizationType;
+
+export const OrganizationConnection = connectionDefinitions({
+  name: "Organization",
+  nodeType: OrganizationType,
+});
